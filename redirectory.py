2f76b2dd3a34b65409c40f9dc04f947f9393bc38
Marc Abramowitz <marc@marc-abramowitz.com>
2013-07-22 13:35:11 -0700

    Make the doctests work

--- redirectory.py
+++ redirectory.py
@@ -5,13 +5,16 @@ from mock import patch
 
 
 def stdin_from(replacement=None):
-    """Redirect stdin to come from a StringIO or str.
+    r"""Redirect stdin to come from a StringIO or str.
 
     'replacement' is a StringIO or str.
 
     Examples:
 
-    >>> from io import stdin_from, StringIO
+    >>> from six import u
+    >>> from six.moves import input
+    >>> from redirectory import stdin_from, StringIO
+
     >>> with stdin_from(StringIO("bleargh")) as s:
     ...     x = input()
     ...
@@ -21,17 +24,18 @@ def stdin_from(replacement=None):
     >>> with stdin_from("bleargh") as s:
     ...     x = input()
     ...
-    >>> x
-    'bleargh'
+    >>> x == u('bleargh')
+    True
     """
     return redirect_file_obj('sys.stdin', replacement)
 
 def stdout_to(replacement=None):
-    """Redirect stdout to a StringIO.
+    r"""Redirect stdout to a StringIO.
 
     Example:
 
-    >>> from io import stdout_to
+    >>> from redirectory import stdout_to
+
     >>> with stdout_to() as s:
     ...     print('hello')
     ...
@@ -42,21 +46,42 @@ def stdout_to(replacement=None):
 
 @contextmanager
 def stdout_to_file(name, mode='w', *open_args, **open_kwargs):
+    r"""Redirect stdout to a file.
+
+    Example:
+
+    >>> import sys
+    >>> from tempfile import NamedTemporaryFile
+    >>> from six import u
+
+    >>> with NamedTemporaryFile(mode='r') as f:
+    ...    # Open file for writing
+    ...    with stdout_to_file(f.name, 'w'):
+    ...        _dummy = sys.stdout.write(u("1 - going to a file\n"))
+    ...    # Open file for appending
+    ...    with stdout_to_file(f.name, 'a'):
+    ...        _dummy = sys.stdout.write(u("2 - going to a file\n"))
+    ...    with open(f.name) as f:
+    ...        data = f.read()
+    ...        data
+    ...
+    '1 - going to a file\n2 - going to a file\n'
+    """
     with open(name, mode, *open_args, **open_kwargs) as file_obj:
         with redirect_file_obj('sys.stdout', file_obj):
             yield
 
 def stderr_to(replacement=None):
-    """Redirect stderr to a StringIO.
+    r"""Redirect stderr to a StringIO.
 
     Example:
 
     >>> import sys
-    >>> from io import stderr_to
+    >>> from redirectory import stderr_to
+
     >>> with stderr_to() as s:
-    ...     print("bleargh", file=sys.stderr)
-    ...
-    >>> s.getvalue()
+    ...     _dummy = sys.stderr.write("bleargh\n")
+    ...     s.getvalue()
     'bleargh\n'
     """
     return redirect_file_obj('sys.stderr', replacement)
